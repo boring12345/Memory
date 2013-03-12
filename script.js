09034a55c48db6fd60bbc4dd406ab03807266ba2
haxor789 <blubb_3@gmx.de>
2013-03-12 17:48:29 +0100

    A first try for a constructor and toggling Cards

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ var addPlayers = function() {
 // To add images using jQuery
 var addImages = function() {
     $('#game_board_frame').append($('<div class="game_board_spacer"></div>'))
-    for (var i = 1; i < 25; i += 1) {
+    for (var i = 1; i < 25; i++) {
         var div = '<div id="card_' + i + '" class="card_frame"><a target="_blank"><img src="badges/code.png" alt="code"></a></div>';
         $('#game_board_frame').append($(div));
         if (i % 6 === 0)
@@ -56,7 +56,54 @@ var addImages = function() {
     }
 };
 
+
+function Image(number,src){
+    this.src = "badges/"+src;
+    this.place = "#card_"+number+ " img";
+}
+
+
 $(document).ready(function(){
     addPlayers();
-    addImages();    
+    addImages();
+
+    var cards = ["23andmeAPI.png", "AddressBook.png", "BitlyAPI.png", "Blackjack.png", "Blackjack2.png", "Blackjack3.png", "BoxAPI.png", "CashRegister.png"/*, "Code.png"*/,
+    "DiceGame.png", "DiceGame2.png", "DwollaAPI.png", "EasyPostAPI.png", "Fifty.png", "FireBaseAPI.png", "First.png", "FiveHundred.png", "FizzBuzz.png", "WePayAPI.png",
+    "FizzBuzz2.png", "Functions.png", "HelloNewYork.png", "HTML5.png", "IfElse.png", "IntroObjects.png", "IntroObjects2.png", "JavascriptAPI.png", "YouTubeAPI.png",
+    "JavascriptIntro.png", "JQuery.png", "KittenAPI.png", "LoyaltyAPI.png", "MandrillAPI.png", "MashapeAPI.png", "OAuth2API.png", "Olympics.png", "OneHundred.png",
+    "OneThousand.png", "OrderInAPI.png", "ParseAPI.png", "PHP.png", "Primitives.png", "Python.png", "PythonAPI.png", "ReviewFunctions.png", "Ruby.png", "RubyAPI.png",
+    "SendGridAPI.png", "SkyDriveAPI.png", "SoundCloudAPI.png", "Startup.png", "Ten.png", "TwentyFive.png", "TwilioAPI.png", "TwitterAPI.png", "TwoHundred.png"];
+    
+    var cardsToHide = [];  
+    var cardsHidden = [];
+
+    for(i=0;i<12;i++){
+	var chosen =Math.floor(Math.random()*cards.length);
+	cardsToHide.push(cards[chosen]);
+	cards.splice(chosen,1);
+    }
+    //------ hkapur97's duplicator------
+    var random = function() {
+        return Math.random() - 0.5;
+    };
+
+    var duplicate = function(array) {
+        return array.concat(array).sort(random);
+    };
+    //----------------------------------
+    cardsToHide = duplicate(cardsToHide);
+    
+
+
+    for(var i =1;i<25;i++){
+	cardsHidden[i-1]= new Image(i,cardsToHide[i-1]);
+    }
+
+
+    $(".card_frame").click(function() {
+	var id = this.id.split("card_").splice(1);
+	var card = cardsHidden[id-1];
+        var src = ($(card.place).attr("src") === "badges/code.png")? card.src: "badges/code.png";
+        $(card.place).attr("src", src);
+    });
 });
