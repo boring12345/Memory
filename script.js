1cc526b62130b6c04a26375681d71982665a0caf
haxor789 <blubb_3@gmx.de>
2013-03-28 18:24:09 +0100

    Update script.js
    added new sets

--- script.js
+++ script.js
@@ -4,7 +4,23 @@
     var pairsFound = 0;
     var noc = 12; // number of cards
     var nop = 4  //number of players
-
+    var all =["23andmeAPI.png", "AddressBook.png", "BitlyAPI.png", "Blackjack.png", "Blackjack2.png", "Blackjack3.png", "BoxAPI.png", "CashRegister.png",
+    	     "DiceGame.png", "DiceGame2.png", "DwollaAPI.png", "EasyPostAPI.png", "Fifty.png", "FireBaseAPI.png", "First.png", "FiveHundred.png", "FizzBuzz.png", "WePayAPI.png",
+    	     "FizzBuzz2.png", "Functions.png", "HelloNewYork.png", "HTML5.png", "IfElse.png", "IntroObjects.png", "IntroObjects2.png", "JavascriptAPI.png", "YouTubeAPI.png",
+   	     "JavascriptIntro.png", "JQuery.png", "KittenAPI.png", "LoyaltyAPI.png", "MandrillAPI.png", "MashapeAPI.png", "OAuth2API.png", "Olympics.png", "OneHundred.png",
+   	     "OneThousand.png", "OrderInAPI.png", "ParseAPI.png", "PHP.png", "Primitives.png", "Python.png", "PythonAPI.png", "ReviewFunctions.png", "Ruby.png", "RubyAPI.png",
+    	     "SendGridAPI.png", "SkyDriveAPI.png", "SoundCloudAPI.png", "Startup.png", "Ten.png", "TwentyFive.png", "TwilioAPI.png", "TwitterAPI.png", "TwoHundred.png"]; 
+    var jsCards= ["Blackjack2.png", "Blackjack3.png", "Blackjack.png", "DiceGame.png", "DiceGame2.png", "FizzBuzz2.png", "FizzBuzz.png", "Functions.png", "HelloNewYork.png",
+ 		 "IfElse.png", "IntroObjects2.png", "IntroObjects.png", "JavascriptAPI.png", "JavascriptIntro.png", "LoyaltyAPI.png", "Olympics.png", "Primitives.png",
+		 "ReviewFunctions.png", "Startup.png", "YouTubeAPI.png"];  // Use the Code.png here
+    var pointCards= ["Fifty.png", "First.png", "FiveHundred.png", "LoyaltyAPI.png", "OneHundred.png", "OneThousand.png", "Ten.png", "TwentyFive.png",
+		    "TwoHundred.png"];  // Use the JQuery.png here
+    var pythonCards = ["Python.png"];  // Use the Python.png here
+    var rubyCards= ["TwitterAPI.png", "BoxAPI.png"];  // Use the Ruby.png here
+    var set = [all,jsCards,pointCards,pythonCards,rubyCards];
+    var setUpCard = ["Code.png","Code.png","JQuery.png","Python.png","Ruby.png"]
+    var upCard;// =setUpCard[chooseSet];
+    var cards =[];// set[chooseSet];
 /*--------------------------------------Clock (not used yet)--------------------------- 
 // Starts the minutes of the clock
 var startMin = function() {
@@ -50,7 +66,7 @@ var addPlayers = function() {
 var addImages = function() {
     $('#game_board_frame').append($('<div class="game_board_spacer"></div>'))
     for (var i = 1; i < 2*noc+1; i++) {
-        var div = '<div id="card_' + i + '" class="card_frame"><a target="_blank"><img src="Badges/Code.png" alt="code"></a></div>';
+        var div = '<div id="card_' + i + '" class="card_frame"><a target="_blank"><img src="Badges/'+upCard+'" alt="code"></a></div>';
         $('#game_board_frame').append($(div));
         if (i % 6 === 0)
             $('#game_board_frame').append($('<div class="game_board_spacer"></div>'))
@@ -78,24 +94,9 @@ function Image(number,src){
         return array.concat(array).sort(random);
     };
 //---------------------------------------------- Hide some cards ------------------------------------------------------------------------
-/* 	var jsCards 	= ["Blackjack2.png", "Blackjack3.png", "Blackjack.png", "DiceGame.png", "DiceGame2.png", "FizzBuzz2.png", "FizzBuzz.png", "Functions.png", "HelloNewYork.png",
-					"IfElse.png", "IntroObjects2.png", "IntroObjects.png", "JavascriptAPI.png", "JavascriptIntro.png", "LoyaltyAPI.png", "Olympics.png", "Primitives.png",
-					"ReviewFunctions.png", "Startup.png", "YouTubeAPI.png"];  // Use the Code.png here
-	var pointCards 	= ["Fifty.png", "First.png", "FiveHundred.png", "LoyaltyAPI.png", "OneHundred.png", "OneThousand.png", "Ten.png", "TwentyFive.png",
-					"TwoHundred.png"];  // Use the JQuery.png here
-	var pythonCards = [];  // Use the Python.png here
-	var rubyCards	= ["TwitterAPI.png", "BoxAPI.png"];  // Use the Ruby.png here
-// This is still a work in progress, as there are still some images in our folder missing. Please start to enable the possibility to add cards (concerning jQuery). :-)
-*/
 
 //creates the array of hidden cards
 function HideCards(){
-    var cards = ["23andmeAPI.png", "AddressBook.png", "BitlyAPI.png", "Blackjack.png", "Blackjack2.png", "Blackjack3.png", "BoxAPI.png", "CashRegister.png",
-    "DiceGame.png", "DiceGame2.png", "DwollaAPI.png", "EasyPostAPI.png", "Fifty.png", "FireBaseAPI.png", "First.png", "FiveHundred.png", "FizzBuzz.png", "WePayAPI.png",
-    "FizzBuzz2.png", "Functions.png", "HelloNewYork.png", "HTML5.png", "IfElse.png", "IntroObjects.png", "IntroObjects2.png", "JavascriptAPI.png", "YouTubeAPI.png",
-    "JavascriptIntro.png", "JQuery.png", "KittenAPI.png", "LoyaltyAPI.png", "MandrillAPI.png", "MashapeAPI.png", "OAuth2API.png", "Olympics.png", "OneHundred.png",
-    "OneThousand.png", "OrderInAPI.png", "ParseAPI.png", "PHP.png", "Primitives.png", "Python.png", "PythonAPI.png", "ReviewFunctions.png", "Ruby.png", "RubyAPI.png",
-    "SendGridAPI.png", "SkyDriveAPI.png", "SoundCloudAPI.png", "Startup.png", "Ten.png", "TwentyFive.png", "TwilioAPI.png", "TwitterAPI.png", "TwoHundred.png"]; 
     var cardsToHide = []; 
     var cardsHidden = []
     for(i=0;i<noc;i++){
@@ -138,6 +139,9 @@ function reset(){// Game over!?
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 
 $(document).ready(function(){
+	var chooseSet = parseInt(prompt("0:all,1:JS,2:PC,3:Py,4:Ru"),10); //Just for the moment 
+    	upCard = setUpCard[chooseSet]; //WARNING: At the moment all Sets with > 12 cards will cause bugs, as no duplicates are allowed!
+    	cards  = set[chooseSet];
 	addImages();
 	addPlayers();
     //get new Cards by pressing start button    
@@ -167,8 +171,8 @@ $(document).on('click',".card_frame",function(){
 		});
 		if(array[0].getSrc() ==array[1].getSrc()){
 			console.log("hit");
-			$(array[0].id).hide(250); //what's best to remove them? Maybe add a class and vanish via css to keep the order?
-			$(array[1].id).hide(250);
+			$(array[0].getPlace()).hide(250); //what's best to remove them? Maybe add a class and vanish via css to keep the order?
+			$(array[1].getPlace()).hide(250);
 			cardsHidden.forEach(function(value,index){ //to avoid scoring points in the 800ms fade out
 				cardsHidden[index].hidden = true;
 			});
@@ -177,7 +181,7 @@ $(document).on('click',".card_frame",function(){
 		}
 		else{
 			setTimeout(function(){
-				$(".card_frame img").attr("src","Badges/Code.png");			
+				$(".card_frame img").attr("src","Badges/"+upCard);			
 				cardsHidden.forEach(function(value,index){
 					cardsHidden[index].hidden = true;
 				});
